3eac9ca648e90fad59d7b107d0e856f29d8ecf4e
uekichi <uekichi@qc.commufa.jp>
2018-10-04 07:52:31 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -2,16 +2,23 @@
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
+
 function fib(n) {
   if (memo.has(n)) {
     return memo.get(n);
   }
-  const value = fib(n - 1) + fib(n - 2);
+  const value = fib(n - 2) + fib(n - 1)
   memo.set(n, value);
   return value;
 }
 
-const length = 40;
-for (let i = 0; i <= length; i++) {
-  console.log(fib(i));
+const n = 40;
+for (let i = 0; i <= n; i++) {
+  console.log(fib(i))
 }
+
+/*
+real	0m4.730s
+user	0m4.696s
+sys	0m0.016s
+*/
