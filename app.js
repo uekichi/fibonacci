82ce1ce8662b4d0ced06c5c718a59024c28510a5
uekicihi <uekichi@qc.commufa.jp>
2020-09-08 20:10:32 +0900

    授業20200908

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 'use strict';
+
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n) {
-  if　(n === 0) {
-    return 0;
-  }　else if (n === 1) {
-    return 1;
+  if　(memo.has(n)) {
+    return memo.get(n);
+  }　else {
+    let value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
   }
-  return fib(n - 1) + fib(n - 2);
 }
 
-const length = 40;
+const length = 100;
 for (let i = 0; i < length; i++) {
   fib(i);
   console.log(i + "のフィボナッチ数は" + fib(i) + "です。");
